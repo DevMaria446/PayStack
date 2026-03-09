d5ec3381521ec6592212b22a915f4fb9c4e17fab
DevMaria446 <mareeahnnadi@gmail.com>
2026-03-09 18:32:44 +0100

    working

---
+++ src/Component/Up/Up.jsx
@@ -0,0 +1,12 @@
+import './Up.css'
+import React from 'react'
+
+const Up = () => {
+  return (
+    <div className='up'>
+      <p className='Reg'> Register your business and get a corporate bank account - fast </p>
+    </div>
+  )
+}
+
+export default Up
